76088284cc2e222821c2a1e05aa11a6f4c16fa8a
Farhan <farhannizami0612@gmail.com>
2024-07-05 02:21:28 +0600

    functions added again

--- src/main/java/com/example/test_runners/run/RunRepository.java
+++ src/main/java/com/example/test_runners/run/RunRepository.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.html.Option;
@@ -24,10 +25,44 @@ public class RunRepository {
     public RunRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
     }
-
     public List<Run> findAll(){
         return jdbcClient.sql("select * from run")
                 .query(Run.class)
                 .list();
+        // The sql can be written in a string var and the use sql(s).query...
+    }
+    public Optional<Run> findById(Integer id){
+        return jdbcClient.sql(
+                "select * from run where id=:id"
+        ).param("id",id).query(Run.class).optional();
+    }
+    public void create(Run run){
+        var created = jdbcClient.sql(
+                "insert into run(id,title,start_on,complete_on,miles,location)"
+                + " values(?,?,?,?,?,?)"
+        ).params(run.id(),run.title(),run.startOn(),run.completeOn(),run.miles(),run.location().toString()).update();
+        Assert.state(created==1,"Failed to create run "+run.title());
+    }
+    public void update(Run run, Integer id){
+        var updated = jdbcClient.sql(
+                "update run set title=?, start_on=?, complete_on=?, miles=?, location=? where id=?"
+        ).params(run.title(),run.startOn(),run.completeOn(),run.miles(),run.location().toString(),id).update();
+        Assert.state(updated==1,"Failed to update run "+run.title());
+    }
+    public void delete(Integer id){
+        var deleted = jdbcClient.sql(
+                "delete from run where id=?"
+        ).param(id).update();
+        Assert.state(deleted==1,"Failed to update run "+id);
+    }
+
+    public int count() {
+        return jdbcClient.sql("select * from run").query().listOfRows().size();
+    }
+    public void saveAll(List<Run> runs){
+        //runs.stream().forEach(this::create);
+        for(Run run: runs){
+            create(run);// easy to understand
+        }
     }
 }
